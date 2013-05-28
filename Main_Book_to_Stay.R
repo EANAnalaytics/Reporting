61999cbc6d32b11960dd3c36d530bb9b50fa376c
crwu <crwu@expedia.com>
2013-05-28 11:22:08 +0200

    Update Main_Book_to_Stay.R
    Corrected Booked Not Stayed to Booked not Stayed

--- Main_Book_to_Stay.R
+++ Main_Book_to_Stay.R
@@ -267,8 +267,8 @@ staydata.sort2<-merge(staydata.sort,PL.assump, by.x = c("Monthtrunc","REGION"), 
 staydata.sort2$Marginnew <- ifelse(staydata.sort2$BMODEL == "Agency", staydata.sort2$GBV*staydata.sort2$AgencyRM,staydata.sort2$MARGIN)
 
 #calculate book not stay minus cancel rate
-staydata.sort2$GBVcan <- ifelse(staydata.sort2$StayType=="Booked Not Stayed",staydata.sort2$GBV*.845,staydata.sort2$GBV)
-staydata.sort2$Margincan <-  ifelse(staydata.sort2$StayType=="Booked Not Stayed",staydata.sort2$Marginnew*.845,staydata.sort2$Marginnew)
+staydata.sort2$GBVcan <- ifelse(staydata.sort2$StayType=="Booked not Stayed",staydata.sort2$GBV*.845,staydata.sort2$GBV)
+staydata.sort2$Margincan <-  ifelse(staydata.sort2$StayType=="Booked not Stayed",staydata.sort2$Marginnew*.845,staydata.sort2$Marginnew)
 
 
 #calc margin for nbns
